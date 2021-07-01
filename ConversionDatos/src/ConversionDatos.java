e1b78a68b322f6791f8086b80dfe02939ae62f98
proivited <miguel.alexis.gomez.a@gmail.com>
2021-07-01 11:26:11 -0500

    Primer commit proyecto Java

---
+++ ConversionDatos/src/ConversionDatos.java
@@ -0,0 +1,30 @@
+
+public class ConversionDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Tipos de datos en Java
+		 * Numericos: byte,short, int, long, float, dousble
+		 * Booleanos )verdadero, falso: boolean
+		 * caracteres: char
+		 * Cadenas: String
+		 * byte almacena de -128 a 127
+		 * int almacena -231 a 231
+		 */
+		byte numeropequeno = 122;
+		byte otronumeropequeno = 122;
+		int suma = numeropequeno + otronumeropequeno;
+		
+		int numero1 = 1000;
+		long numero2 = numero1;
+		float numero3 = numero2;
+		
+		System.out.println(numero1);
+		System.out.println(numero2);
+		System.out.println(numero3);
+		System.out.println(suma);
+
+	}
+
+}
